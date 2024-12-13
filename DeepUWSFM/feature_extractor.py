7a12c33ae0940d3a3cbf529c173945ceab04cfe9
carterweaver <carter@jars.ai>
2024-12-12 23:56:07 -0500

    refactor feature extractor

--- DeepUWSFM/feature_extractor.py
+++ DeepUWSFM/feature_extractor.py
@@ -13,108 +13,35 @@ from types import SimpleNamespace
 from typing import Dict, List, Optional, Union
 
 from . import extractors
-# from . import superpoint_wrapper
 from .utils.base_model import dynamic_load
 from .utils.io import list_h5_names, read_image
 from .utils.parsers import parse_image_lists
+from .image_dataset import ImgDataset
+from .config import configs
 
-# configs for different models
-configs = {
-    "superpoint": {
-        "out_dir": "superpoint-feats-n4096-r1024",
-        "model_stuff": {
-            "name": "superpoint",
-            "nms_radius": 3,
-            "max_kp": 4096,
-        },
-        "preprocess": {
-            "gray": True,
-            "max_size": 1024,
-        },
-    },
-    "sift": {
-        "output": "feats-sift",
-        "model": {"name": "dog"},
-        "preprocessing": {
-            "grayscale": True,
-            "resize_max": 1600,
-        },
-    },
-}
-
-def resize_img(img, size, method):
-    if method.startswith('cv2_'):
-        m = getattr(cv2, 'INTER_' + method[4:].upper())
-        h, w = img.shape[:2]
-        if m == cv2.INTER_AREA and (w < size[0] or h < size[1]):
-            m = cv2.INTER_LINEAR
-        new_img = cv2.resize(img, size, interpolation=m)
-    elif method.startswith('pil_'):
-        m = getattr(PIL.Image, method[4:].upper())
-        new_img = PIL.Image.fromarray(img.astype(np.uint8))
-        new_img = new_img.resize(size, resample=m)
-        new_img = np.asarray(new_img, dtype=img.dtype)
-    else:
-        raise ValueError(f"Bad resize method: {method}")
-    return new_img
-
-class ImgDataset(torch.utils.data.Dataset):
-    defaults = {
-        "patterns": ["*.jpg", "*.png", "*.jpeg", "*.JPG", "*.PNG"],
-        "gray": False,
-        "max_size": None,
-        "force_resize": False,
-        "resize_method": "cv2_area",
-    }
-
-    def __init__(self, root, cfg, img_list=None):
-        self.cfg = SimpleNamespace(**{**self.defaults, **cfg})
-        self.root = root
-
-        if not img_list:
-            paths = []
-            for pat in self.cfg.patterns:
-                paths += glob.glob((Path(root)/"**"/pat).as_posix(), recursive=True)
-            if not paths:
-                raise ValueError(f"No images found in {root}")
-            paths = sorted(set(paths))
-            self.imgs = [Path(p).relative_to(root).as_posix() for p in paths]
-            # logger.info(f"Found {len(self.imgs)} images in {root}")
-        else:
-            if isinstance(img_list, (Path, str)):
-                self.imgs = parse_image_lists(img_list)
-            elif isinstance(img_list, colls.Iterable):
-                self.imgs = [p.as_posix() if isinstance(p, Path) else p for p in img_list]
-            else:
-                raise ValueError(f"Bad img_list format: {img_list}")
-
-            for img in self.imgs:
-                if not (root/img).exists():
-                    raise ValueError(f"Image {img} not found in {root}")
-
-    def __getitem__(self, idx):
-        img = read_image(self.root/self.imgs[idx], self.cfg.gray)
-        img = img.astype(np.float32)
-        size = img.shape[:2][::-1]
-
-        if self.cfg.max_size and (self.cfg.force_resize or max(size) > self.cfg.max_size):
-            scale = self.cfg.max_size / max(size)
-            new_size = tuple(int(round(x*scale)) for x in size)
-            img = resize_img(img, new_size, self.cfg.resize_method)
-
-        if self.cfg.gray:
-            img = img[None]
-        else:
-            img = img.transpose((2,0,1))
-        img = img/255.0
-
-        return {
-            "img": img,
-            "orig_size": np.array(size),
-        }
-
-    def __len__(self):
-        return len(self.imgs)
+# # configs for different models
+# configs = {
+#     "superpoint": {
+#         "out_dir": "superpoint-feats-n4096-r1024",
+#         "model_stuff": {
+#             "name": "superpoint",
+#             "nms_radius": 3,
+#             "max_kp": 4096,
+#         },
+#         "preprocess": {
+#             "gray": True,
+#             "max_size": 1024,
+#         },
+#     },
+#     "sift": {
+#         "output": "feats-sift",
+#         "model": {"name": "dog"},
+#         "preprocessing": {
+#             "grayscale": True,
+#             "resize_max": 1600,
+#         },
+#     },
+# }
 
 @torch.no_grad()
 def extract_features(
@@ -126,7 +53,6 @@ def extract_features(
     feat_path: Optional[Path] = None,
     overwrite: bool = False,
 ) -> Path:
-    # logger.info(f"Extracting features with config:\n{pprint.pformat(cfg)}")
 
     data = ImgDataset(img_dir, cfg["preprocess"], img_list)
     if not feat_path:
@@ -136,7 +62,6 @@ def extract_features(
     done_imgs = set(list_h5_names(feat_path) if feat_path.exists() and not overwrite else ())
     data.imgs = [n for n in data.imgs if n not in done_imgs]
     if not data.imgs:
-        # logger.info("Nothing to do")
         return feat_path
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -175,29 +100,102 @@ def extract_features(
                     g["keypoints"].attrs["uncertainty"] = uncert
             except OSError as e:
                 if "No space left on device" in e.args[0]:
-                    # logger.error("Disk full! Try using half precision")
                     del g, f[name]
                 raise e
 
         del out
-
-    # logger.info("Done!")
     return feat_path
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--img_dir", type=Path, required=True)
-    parser.add_argument("--out_dir", type=Path, required=True)
-    parser.add_argument("--cfg", type=str, default="superpoint", choices=list(configs.keys()))
-    parser.add_argument("--half", action="store_true")
-    parser.add_argument("--img_list", type=Path)
-    parser.add_argument("--feat_path", type=Path)
-    args = parser.parse_args()
-    extract_features(
-        configs[args.cfg],
-        args.img_dir,
-        args.out_dir,
-        args.half,
-        args.img_list,
-        args.feat_path,
-    )
+def resize_img(img, size, method):
+    if method.startswith('cv2_'):
+        m = getattr(cv2, 'INTER_' + method[4:].upper())
+        h, w = img.shape[:2]
+        if m == cv2.INTER_AREA and (w < size[0] or h < size[1]):
+            m = cv2.INTER_LINEAR
+        new_img = cv2.resize(img, size, interpolation=m)
+    elif method.startswith('pil_'):
+        m = getattr(PIL.Image, method[4:].upper())
+        new_img = PIL.Image.fromarray(img.astype(np.uint8))
+        new_img = new_img.resize(size, resample=m)
+        new_img = np.asarray(new_img, dtype=img.dtype)
+    else:
+        raise ValueError(f"Bad resize method: {method}")
+    return new_img
+
+# class ImgDataset(torch.utils.data.Dataset):
+#     defaults = {
+#         "patterns": ["*.jpg", "*.png", "*.jpeg", "*.JPG", "*.PNG"],
+#         "gray": False,
+#         "max_size": None,
+#         "force_resize": False,
+#         "resize_method": "cv2_area",
+#     }
+
+#     def __init__(self, root, cfg, img_list=None):
+#         self.cfg = SimpleNamespace(**{**self.defaults, **cfg})
+#         self.root = root
+
+#         if not img_list:
+#             paths = []
+#             for pat in self.cfg.patterns:
+#                 paths += glob.glob((Path(root)/"**"/pat).as_posix(), recursive=True)
+#             if not paths:
+#                 raise ValueError(f"No images found in {root}")
+#             paths = sorted(set(paths))
+#             self.imgs = [Path(p).relative_to(root).as_posix() for p in paths]
+#             # logger.info(f"Found {len(self.imgs)} images in {root}")
+#         else:
+#             if isinstance(img_list, (Path, str)):
+#                 self.imgs = parse_image_lists(img_list)
+#             elif isinstance(img_list, colls.Iterable):
+#                 self.imgs = [p.as_posix() if isinstance(p, Path) else p for p in img_list]
+#             else:
+#                 raise ValueError(f"Bad img_list format: {img_list}")
+
+#             for img in self.imgs:
+#                 if not (root/img).exists():
+#                     raise ValueError(f"Image {img} not found in {root}")
+
+#     def __getitem__(self, idx):
+#         img = read_image(self.root/self.imgs[idx], self.cfg.gray)
+#         img = img.astype(np.float32)
+#         size = img.shape[:2][::-1]
+
+#         if self.cfg.max_size and (self.cfg.force_resize or max(size) > self.cfg.max_size):
+#             scale = self.cfg.max_size / max(size)
+#             new_size = tuple(int(round(x*scale)) for x in size)
+#             img = resize_img(img, new_size, self.cfg.resize_method)
+
+#         if self.cfg.gray:
+#             img = img[None]
+#         else:
+#             img = img.transpose((2,0,1))
+#         img = img/255.0
+
+#         return {
+#             "img": img,
+#             "orig_size": np.array(size),
+#         }
+
+#     def __len__(self):
+#         return len(self.imgs)
+
+
+
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("--img_dir", type=Path, required=True)
+#     parser.add_argument("--out_dir", type=Path, required=True)
+#     parser.add_argument("--cfg", type=str, default="superpoint", choices=list(configs.keys()))
+#     parser.add_argument("--half", action="store_true")
+#     parser.add_argument("--img_list", type=Path)
+#     parser.add_argument("--feat_path", type=Path)
+#     args = parser.parse_args()
+#     extract_features(
+#         configs[args.cfg],
+#         args.img_dir,
+#         args.out_dir,
+#         args.half,
+#         args.img_list,
+#         args.feat_path,
+#     )
---
+++ DeepUWSFM/image_dataset.py
@@ -0,0 +1,72 @@
+import argparse
+import collections.abc as colls
+import glob
+import pprint
+from pathlib import Path
+import cv2
+import h5py
+import numpy as np
+import PIL.Image
+import torch
+from types import SimpleNamespace
+from typing import Dict, List, Optional, Union
+
+from .utils.io import read_image
+from .utils.parsers import parse_image_lists
+
+class ImgDataset(torch.utils.data.Dataset):
+  defaults = {
+      "patterns": ["*.jpg", "*.png", "*.jpeg", "*.JPG", "*.PNG"],
+      "gray": False,
+      "max_size": None,
+      "force_resize": False,
+      "resize_method": "cv2_area",
+  }
+
+  def __init__(self, root, cfg, img_list=None):
+      self.cfg = SimpleNamespace(**{**self.defaults, **cfg})
+      self.root = root
+
+      if not img_list:
+          paths = []
+          for pat in self.cfg.patterns:
+              paths += glob.glob((Path(root)/"**"/pat).as_posix(), recursive=True)
+          if not paths:
+              raise ValueError(f"No images found in {root}")
+          paths = sorted(set(paths))
+          self.imgs = [Path(p).relative_to(root).as_posix() for p in paths]
+      else:
+          if isinstance(img_list, (Path, str)):
+              self.imgs = parse_image_lists(img_list)
+          elif isinstance(img_list, colls.Iterable):
+              self.imgs = [p.as_posix() if isinstance(p, Path) else p for p in img_list]
+          else:
+              raise ValueError(f"Bad img_list format: {img_list}")
+
+          for img in self.imgs:
+              if not (root/img).exists():
+                  raise ValueError(f"Image {img} not found in {root}")
+
+  def __getitem__(self, idx):
+      img = read_image(self.root/self.imgs[idx], self.cfg.gray)
+      img = img.astype(np.float32)
+      size = img.shape[:2][::-1]
+
+      if self.cfg.max_size and (self.cfg.force_resize or max(size) > self.cfg.max_size):
+          scale = self.cfg.max_size / max(size)
+          new_size = tuple(int(round(x*scale)) for x in size)
+          img = resize_img(img, new_size, self.cfg.resize_method)
+
+      if self.cfg.gray:
+          img = img[None]
+      else:
+          img = img.transpose((2,0,1))
+      img = img/255.0
+
+      return {
+          "img": img,
+          "orig_size": np.array(size),
+      }
+
+  def __len__(self):
+      return len(self.imgs)
